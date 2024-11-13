c5eda958a5d5703a8d02d9e34fd4007044086deb
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2024-11-13 12:27:55 +0100

    function createPokemons

---
+++ src/interfaces/IPokemon.ts
@@ -0,0 +1,11 @@
+export interface IPokemon {
+    count: number;
+    next: string;
+    previous: null;
+    results: IResults[]
+}
+
+export interface IResults {
+    name: string;
+    url: string
+}
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,29 @@
 import './style.css'
-console.log("hi");
+
+import { IResults } from './interfaces/IPokemon';
+import { IType } from './interfaces/IPokemonInfo';
+
+
+const BASE_URL = "https://pokeapi.co";
+
+const pokemonInput = document.getElementById('pokemonInput') as HTMLInputElement;
+const btn = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>;
+const pokemonList = document.getElementById('pokemonList') as HTMLDivElement;
+
+function createPokemons(pokemon: IResults){
+  const newsPokemon = document.createElement('div') as HTMLDivElement;
+
+  newsPokemon.innerHTML = `
+    <img src="" alt="${pokemon.name}">
+    <div>
+      
+    </div>
+    <p>
+      ${pokemon.name}
+    </p>
+  `
+
+  pokemonList.appendChild(newsPokemon)
+}
+
+
